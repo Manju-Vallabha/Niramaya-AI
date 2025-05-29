5a9cdb62ec61061313459574fdb1a1777068a7e3
Manju Vallabha Pavalla <143684911+Manju-Vallabha@users.noreply.github.com>
2025-05-29 17:15:44 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -505,7 +505,7 @@ const Home = () => {
           </p>
         </div>
       </motion.section>
-
+      { /*
       <motion.section
         className="py-16 px-4 sm:px-8 max-w-7xl mx-auto bg-background"
         variants={containerVariants}
@@ -513,7 +513,7 @@ const Home = () => {
         animate="visible"
         aria-labelledby="team-heading"
       >
-{/*         <h2 id="team-heading" className="text-3xl sm:text-4xl font-bold text-primary text-center mb-4">
+        <h2 id="team-heading" className="text-3xl sm:text-4xl font-bold text-primary text-center mb-4">
           Meet The AI-nfluencers
         </h2>
         <p className="text-gray-600 text-base sm:text-lg text-center mb-10 max-w-3xl mx-auto">
